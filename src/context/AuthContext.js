79bf9aa81c8ebc7ae26ce58d5c3ea28ef110950d
Raluca Cigmăian <ralucacigmaian@Ralucas-MacBook-Pro.local>
2023-04-30 19:27:54 +0300

    Added Notifications

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -7,10 +7,12 @@ export const UserContext = createContext({
   fid: "",
   userName: "",
   friendName: "",
+  expoPushToken: "",
   getUserId: () => {},
   getFriendId: () => {},
   getUserName: () => {},
   getFriendName: () => {},
+  getExpoPushToken: () => {},
   logout: () => {},
 });
 
@@ -19,6 +21,7 @@ function UserContextProvider({ children }) {
   const [friendId, setFriendId] = useState(null);
   const [userName, setUserName] = useState(null);
   const [friendName, setFriendName] = useState(null);
+  const [expoPushToken, setExpoPushToken] = useState(null);
 
   useEffect(() => {
     async function fetchUserId() {
@@ -60,6 +63,16 @@ function UserContextProvider({ children }) {
     fetchFriendName();
   }, []);
 
+  useEffect(() => {
+    async function fetchExpoPushToken() {
+      const storeExpoPushToken = await AsyncStorage.getItem("expoPushToken");
+      if (storeExpoPushToken) {
+        setExpoPushToken(storeExpoPushToken);
+      }
+    }
+    fetchExpoPushToken();
+  }, []);
+
   function getUserId(id) {
     setUserId(id);
     AsyncStorage.setItem("uid", id);
@@ -80,6 +93,11 @@ function UserContextProvider({ children }) {
     AsyncStorage.setItem("friendName", name);
   }
 
+  function getExpoPushToken(token) {
+    setExpoPushToken(token);
+    AsyncStorage.setItem("expoPushToken", token);
+  }
+
   function logout() {
     setUserId(null);
   }
@@ -88,10 +106,12 @@ function UserContextProvider({ children }) {
     fid: friendId,
     userName: userName,
     friendName: friendName,
+    expoPushToken: expoPushToken,
     getUserId: getUserId,
     getFriendId: getFriendId,
     getUserName: getUserName,
     getFriendName: getFriendName,
+    getExpoPushToken: getExpoPushToken,
     logout: logout,
   };
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
