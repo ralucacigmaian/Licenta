2e98186792a6c5a55eb37531d757567c9b85e25d
Raluca Cigmăian <ralucacigmaian@192-168-0-100.rdsnet.ro>
2023-06-13 01:10:44 +0300

    Added SearchBar Component for Event List

--- src/screens/EventsScreen.js
+++ src/screens/EventsScreen.js
@@ -7,6 +7,7 @@ import EventCard from "../components/EventCard";
 import { Colors } from "../utils/colors";
 import LottieView from "lottie-react-native";
 import { ScrollView } from "react-native-gesture-handler";
+import SearchBarComponent from "../components/SearchBarComponent";
 
 function EventsScreen() {
   const authenticatedUser = useContext(UserContext);
@@ -57,54 +58,235 @@ function EventsScreen() {
     setEventsArray((prevEvents) => prevEvents.filter((x) => x.id !== idEvent));
   };
 
+  const [searchInput, setSearchInput] = useState("");
+  const [searchEventsArray, setSearchEventsArray] = useState([]);
+  const [searchStatus, setSearchStatus] = useState(false);
+
+  const handleSearchInput = useCallback((text) => {
+    setSearchInput(text);
+
+    if (text === "") {
+      setSearchEventsArray([]);
+      setSearchStatus(false);
+      return;
+    }
+
+    const searchedEvents = eventsArray.filter(
+      (event) =>
+        event.eventType.toLowerCase().includes(text.toLowerCase()) ||
+        event.name1.toLowerCase().includes(text.toLowerCase()) ||
+        event.name2.toLowerCase().includes(text.toLowerCase()) ||
+        event.eventLocation.toLowerCase().includes(text.toLowerCase())
+    );
+
+    setSearchEventsArray(searchedEvents);
+
+    const hasSearchResults = searchedEvents.length;
+
+    setSearchStatus(hasSearchResults);
+  });
+
+  console.log(searchStatus);
+
   if (loading) return <ActivityIndicator />;
 
-  return (
-    <View style={styles.container}>
-      {eventsArray.length > 0 && (
-        <Text style={styles.textEvents}>Evenimente</Text>
-      )}
-      {eventsArray.length > 0 ? (
-        <ScrollView style={styles.scrollView}>
-          {eventsArray.map((x) => {
-            return (
-              <View key={x.id} style={styles.containerEvent}>
-                <EventCard
-                  idUser={authenticatedUser.uid}
-                  idEvent={x.id}
-                  eventType={x.eventType}
-                  name1={x.name1}
-                  name2={x.name2}
-                  date={x.eventDate}
-                  hour={x.eventHour}
-                  location={x.eventLocation}
-                  hasPassed={x.hasPassed}
-                  hasMemory={x.hasMemory}
-                  imageMemory={x.imageMemory}
-                  onDelete={handleDeleteEvent}
-                />
-              </View>
-            );
-          })}
+  if (searchStatus === false) {
+    return (
+      <View style={styles.container}>
+        <SearchBarComponent
+          placeholder="Caută în lista de evenimente"
+          value={searchInput}
+          onChangeText={handleSearchInput}
+        />
+        <ScrollView>
+          {eventsArray.length > 0 && (
+            <Text style={styles.textEvents}>Evenimente din trecut</Text>
+          )}
+          {eventsArray.length > 0 && (
+            <View>
+              {eventsArray.map((x) => {
+                if (x.hasPassed === 1) {
+                  return (
+                    <View key={x.id} style={styles.containerEvent}>
+                      <EventCard
+                        idUser={authenticatedUser.uid}
+                        idEvent={x.id}
+                        eventType={x.eventType}
+                        name1={x.name1}
+                        name2={x.name2}
+                        date={x.eventDate}
+                        hour={x.eventHour}
+                        location={x.eventLocation}
+                        hasPassed={x.hasPassed}
+                        hasMemory={x.hasMemory}
+                        imageMemory={x.imageMemory}
+                        onDelete={handleDeleteEvent}
+                      />
+                    </View>
+                  );
+                }
+              })}
+            </View>
+          )}
+          {eventsArray.length > 0 && (
+            <Text style={styles.textFutureEvents}>Evenimente viitoare</Text>
+          )}
+          {eventsArray.length > 0 && (
+            <View>
+              {eventsArray.map((x) => {
+                if (x.hasPassed === 0) {
+                  return (
+                    <View key={x.id} style={styles.containerEvent}>
+                      <EventCard
+                        idUser={authenticatedUser.uid}
+                        idEvent={x.id}
+                        eventType={x.eventType}
+                        name1={x.name1}
+                        name2={x.name2}
+                        date={x.eventDate}
+                        hour={x.eventHour}
+                        location={x.eventLocation}
+                        hasPassed={x.hasPassed}
+                        hasMemory={x.hasMemory}
+                        imageMemory={x.imageMemory}
+                        onDelete={handleDeleteEvent}
+                      />
+                    </View>
+                  );
+                }
+              })}
+            </View>
+          )}
         </ScrollView>
-      ) : (
-        <View style={styles.containerNoEvents}>
-          <Text style={styles.textNoEvents}>
-            Ne pare rău, nu există evenimente adăugate!
-          </Text>
-          <LottieView
-            source={require("../utils/noEvents.json")}
-            autoPlay
-            loop
-            style={{
-              width: 200,
-              height: 200,
-            }}
+        {eventsArray.length === 0 && (
+          <View style={styles.containerNoEvents}>
+            <Text style={styles.textNoEvents}>
+              Ne pare rău, nu există evenimente adăugate!
+            </Text>
+            <LottieView
+              source={require("../utils/noEvents.json")}
+              autoPlay
+              loop
+              style={{
+                width: 200,
+                height: 200,
+              }}
+            />
+          </View>
+        )}
+      </View>
+    );
+  } else {
+    if (searchStatus === 0) {
+      return (
+        <View style={styles.container}>
+          <SearchBarComponent
+            placeholder="Caută în lista de evenimente"
+            value={searchInput}
+            onChangeText={handleSearchInput}
           />
+          <View style={styles.containerNoResult}>
+            <Text style={styles.textNoResult}>
+              Ne pare rău, dar nu am găsit nicio înregistrare care să corespundă
+              căutării tale!
+            </Text>
+            <LottieView
+              source={require("../utils/search_empty.json")}
+              autoPlay
+              loop
+              style={{
+                width: 300,
+                height: 300,
+              }}
+            />
+          </View>
         </View>
-      )}
-    </View>
-  );
+      );
+    } else {
+      if (searchStatus !== 0) {
+        return (
+          <View style={styles.container}>
+            <SearchBarComponent
+              placeholder="Caută în lista de evenimente"
+              value={searchInput}
+              onChangeText={handleSearchInput}
+            />
+            {searchEventsArray.length > 0 &&
+              searchEventsArray.filter((x) => x.hasPassed === 1) && (
+                <View>
+                  <Text style={styles.textEvents}>Evenimente din trecut</Text>
+                  {searchEventsArray.filter((x) => x.hasPassed === 1).length ===
+                    0 && (
+                    <Text style={styles.textNoPastEvents}>
+                      Nu există rezultate corespunzătoare evenimentelor trecute!
+                    </Text>
+                  )}
+                  {searchEventsArray.map((x) => {
+                    if (x.hasPassed === 1) {
+                      return (
+                        <View key={x.id} style={styles.containerEvent}>
+                          <EventCard
+                            idUser={authenticatedUser.uid}
+                            idEvent={x.id}
+                            eventType={x.eventType}
+                            name1={x.name1}
+                            name2={x.name2}
+                            date={x.eventDate}
+                            hour={x.eventHour}
+                            location={x.eventLocation}
+                            hasPassed={x.hasPassed}
+                            hasMemory={x.hasMemory}
+                            imageMemory={x.imageMemory}
+                            onDelete={handleDeleteEvent}
+                          />
+                        </View>
+                      );
+                    }
+                  })}
+                </View>
+              )}
+            {searchEventsArray.length > 0 &&
+              searchEventsArray.filter((x) => x.hasPassed === 0) && (
+                <View>
+                  <Text style={styles.textFutureEvents}>
+                    Evenimente viitoare
+                  </Text>
+                  {searchEventsArray.filter((x) => x.hasPassed === 0).length ===
+                    0 && (
+                    <Text style={styles.textNoPastEvents}>
+                      Nu există rezultate corespunzătoare evenimentelor
+                      viitoare!
+                    </Text>
+                  )}
+                  {searchEventsArray.map((x) => {
+                    if (x.hasPassed === 0) {
+                      return (
+                        <View key={x.id} style={styles.containerEvent}>
+                          <EventCard
+                            idUser={authenticatedUser.uid}
+                            idEvent={x.id}
+                            eventType={x.eventType}
+                            name1={x.name1}
+                            name2={x.name2}
+                            date={x.eventDate}
+                            hour={x.eventHour}
+                            location={x.eventLocation}
+                            hasPassed={x.hasPassed}
+                            hasMemory={x.hasMemory}
+                            imageMemory={x.imageMemory}
+                            onDelete={handleDeleteEvent}
+                          />
+                        </View>
+                      );
+                    }
+                  })}
+                </View>
+              )}
+          </View>
+        );
+      }
+    }
+  }
 }
 
 const styles = StyleSheet.create({
@@ -117,6 +299,13 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: Colors.colors.darkDustyPurple,
     marginLeft: 16,
+  },
+  textFutureEvents: {
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 20,
+    color: Colors.colors.darkDustyPurple,
+    marginLeft: 16,
+    marginTop: 16,
   },
   containerEvents: {
     flex: 1,
@@ -139,6 +328,25 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: Colors.colors.darkDustyPurple,
     textAlign: "center",
+  },
+  containerNoResult: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    marginHorizontal: 16,
+  },
+  textNoResult: {
+    fontFamily: "Montserrat-SemiBold",
+    fontSize: 20,
+    color: Colors.colors.darkDustyPurple,
+    textAlign: "center",
+  },
+  textNoPastEvents: {
+    fontFamily: "Montserrat-Regular",
+    fontSize: 16,
+    color: Colors.colors.darkDustyPurple,
+    marginLeft: 16,
+    marginTop: 16,
   },
 });
 
